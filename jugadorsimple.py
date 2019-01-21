b5beb73ad92b6c9ccb1d98bcfa7ec7fec6f8c2ee
Antonio Manuel Milla Lara <amillalara@hotmail.com>
2019-01-21 19:39:54 +0100

    Add files via upload

--- jugadorsimple.py
+++ jugadorsimple.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam, Simulation, show_simu
+from soccersimulator.settings import GAME_WIDTH, GAME_HEIGHT, PLAYER_RADIUS, BALL_RADIUS
 
 class EstrategiaSimple(Strategy):
 	def __init__(self):
-		Strategy.__init__(self, "Random")
+		Strategy.__init__(self, "Simple")
 
 	def compute_strategy(self, state, id_team, id_player):
 		# id_team is 1 or 2
@@ -11,11 +12,12 @@ class EstrategiaSimple(Strategy):
 		posJug = state.player_state(id_team,id_player).position
 		posBal = state.ball.position
 
-		But1 = Vector2D(0,90/2.)
-		But2 = Vector2D(150,90/2.)
+		But1 = Vector2D(0,GAME_HEIGHT/2.)
+		But2 = Vector2D(GAME_WIDTH,GAME_HEIGHT/2.)
+		DistanceBal = PLAYER_RADIUS+BALL_RADIUS
 
-		if posJug.distance(posBal) > 1.65:
-			return SoccerAction(posBal-posJug, posBal-posJug)
+		if posJug.distance(posBal) > DistanceBal:
+			return SoccerAction(posBal-posJug, 0.)
 		else:
 			if id_team == 1:
 				return SoccerAction(But2-posJug,But2-posJug)
@@ -28,8 +30,8 @@ team2 = SoccerTeam(name="Barcelona")
 
 # Add players
 team1.add("Jugador Simple", EstrategiaSimple())  # Simple strategy
-team2.add("Jugador Estatico", Strategy())   # Static strategy
-#team2.add("Jugador Simple", EstrategiaSimple())
+#team2.add("Jugador Estatico", Strategy())   # Static strategy
+team2.add("Jugador Simple", EstrategiaSimple())
 
 # Create a match
 simu = Simulation(team1, team2)
